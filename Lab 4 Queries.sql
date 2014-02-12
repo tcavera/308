6def2f26a190fe3dcacef65db29aed1d926e718e
tcavera <tcavera@gmail.com>
2014-02-12 00:20:34 -0500

    Lab 4 for Professor Labouseur.
    SQL file for Lab 4

---
+++ Lab 4 Queries.sql
@@ -0,0 +1,53 @@
+﻿--Tyler Cavera 
+--SQL file for Lab 4
+--QUERY 1
+select city
+from  agents
+where aid in (select aid
+	      from orders
+	      where cid in (select cid
+		            from customers
+		            where name = 'Basics'));
+--QUERY 2
+select distinct pid
+from orders
+where aid in (select aid
+              from orders
+              where cid in (select cid
+			    from customers 
+			    where city = 'Kyoto'));
+--QUERY 3
+select cid, name
+from customers
+where cid not in (select cid
+                  from orders
+                  where aid = 'a03');
+--QUERY 4
+select cid, name
+from customers
+where cid in (select cid
+              from orders
+              where pid = 'p01'
+              or pid = 'p07');
+--QUERY 5
+select distinct pid
+from orders
+where cid in (select cid
+              from orders
+              where aid = 'a03');
+--QUERY 6
+select name, discount
+from customers
+where cid in (select cid
+	      from orders
+	      where aid in (select aid
+	                    from agents
+	                    where city = 'Dallas'
+	                    or city = 'Duluth'));
+--QUERY 7
+select name
+from customers
+where discount in (select discount 
+                   from customers
+                   where city = 'Dallas'
+                   or city = 'Kyoto');
