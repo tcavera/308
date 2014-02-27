6bc12e6fb64370e8473f151c2ee690edc1417021
tcavera <tcavera@gmail.com>
2014-02-26 23:12:34 -0500

    Lab 6 Queries
    Not completed yet.

---
+++ Lab 6 Queries.sql
@@ -0,0 +1,26 @@
+﻿--Tyler Cavera
+--2/26/14
+--SQL file for Lab 6
+--a work in progress
+
+--QUERY 1
+
+
+--QUERY 2
+
+
+--QUERY 3 
+select priceUSD, name
+from products 
+where priceUSD > (select avg(priceUSD)
+                    from products);
+
+--QUERY 4
+select orders.cid, orders.pid, orders.dollars, customers.name 
+from orders, customers
+where (customers.cid = orders.cid)
+order by dollars desc
+
+--QUERY 5
+
+  
