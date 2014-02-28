b7a98f08dd6da54cbcb0d003b4efe7645600291a
tcavera <tcavera@gmail.com>
2014-02-27 23:29:30 -0500

    Update Lab 6 Queries
    (Correctly, hopefully) completed more queries for Lab 6.

--- Lab 6 Queries.sql
+++ Lab 6 Queries.sql
@@ -19,8 +19,25 @@ where priceUSD > (select avg(priceUSD)
 select orders.cid, orders.pid, orders.dollars, customers.name 
 from orders, customers
 where (customers.cid = orders.cid)
-order by dollars desc
+order by dollars desc;
 
 --QUERY 5
+select customers.name, COUNT(orders.ordno) orderTotal
+from orders LEFT JOIN customers
+on customers.cid = orders.cid
+group by customers.name;
 
-  
+
+--QUERY 6
+select customers.name as customer, products.name as product, agents.name as agent
+from customers, agents, products, orders
+where (customers.cid = orders.cid)
+  and (orders.pid = products.pid)
+  and (orders.aid = agents.aid)
+  and (agents.city = 'New York');
+
+--QUERY 7
+select orders.ordno, orders.dollars, ((orders.qty * products.priceUSD) - ((orders.qty * products.priceUSD) * (customers.discount / 100))) as orderCheck 
+from customers, orders, products
+where (customers.cid = orders.cid)
+  and (orders.pid = products.pid);
